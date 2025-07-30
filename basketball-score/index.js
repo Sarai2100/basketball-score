2eb603a520a53de7bb39013c406233165a4b4384
Sara <sara.miromano@gmail.com>
2025-07-30 12:58:22 -0400

    Added proper js

--- basketball-score/index.js
+++ basketball-score/index.js
@@ -1,45 +1,31 @@
-let homeScore = document.querySelector(".home-score")
-let guestScore = document.querySelector(".guest-score")
-let homeCount = 0
-let guestCount = 0
+let homeScore = document.querySelector('.home-score');
+let guestScore = document.querySelector('.guest-score');
 
-function incrementHome1() {
-  homeCount += 1
-  homeScore.textContent = homeCount 
-  
-  
-}
+let homeCount = 0;
+let guestCount = 0;
 
-function incrementHome2() {
-  homeCount += 2
-  homeScore.textContent = homeCount 
-  
-  
-}
+document.getElementById('home-1').addEventListener('click', () => {
+  homeCount += 1;
+  homeScore.textContent = homeCount;
+});
+document.getElementById('home-2').addEventListener('click', () => {
+  homeCount += 2;
+  homeScore.textContent = homeCount;
+});
+document.getElementById('home-3').addEventListener('click', () => {
+  homeCount += 3;
+  homeScore.textContent = homeCount;
+});
 
-function incrementHome3() {
-  homeCount += 3
-  homeScore.textContent = homeCount 
- 
-  
-}
-
-
-function incrementGuest1() {
-  guestCount += 1
-  guestScore.textContent = guestCount
-  
-}
-
-function incrementGuest2() {
-  guestCount += 2
-  guestScore.textContent = guestCount
-  
-}
-
-function incrementGuest3() {
-  guestCount += 3
-  guestScore.textContent = guestCount
-  
-  
-}
+document.getElementById('guest-1').addEventListener('click', () => {
+  guestCount += 1;
+  guestScore.textContent = guestCount;
+});
+document.getElementById('guest-2').addEventListener('click', () => {
+  guestCount += 2;
+  guestScore.textContent = guestCount;
+});
+document.getElementById('guest-3').addEventListener('click', () => {
+  guestCount += 3;
+  guestScore.textContent = guestCount;
+});
